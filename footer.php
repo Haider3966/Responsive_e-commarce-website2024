ff7e4d8258f1d725fce4f3fa01365715c9544429
Ashraf Haider <138344094+Haider3966@users.noreply.github.com>
2024-05-25 07:09:56 +0600

    Components

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:01770658682 "><i class="fas fa-phone"></i>+8801790658682</a>
+         <a href="tel:01770658682 "><i class="fas fa-phone"></i>+8801644428730</a>
+         <a href=" haider15-3966@diu.edu.bd"><i class="fas fa-envelope"></i>haider15-3966@diu.edu.bd</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i>Gazipur,Dhaka,Bangladesh  </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href=" https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href=" " target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/muhammad_ashraf_haider/" target="_blank"><i class=" "></i>Instagram</a>
+         <a href=" https://www.linkedin.com/in/muhammad-ashraf-haider-a626572b7/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ASHRAF HAIDER</span> | all rights reserved!</div>
+
+</footer>
